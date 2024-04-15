911851ba4b9d064d5d596006efbb849f7e4b82f6
Axel Colmant <axel.colmant@gmail.com>
2024-04-15 14:20:08 +0200

    Create Chess class

---
+++ chess/chess.py
@@ -0,0 +1,13 @@
+from .board.board import Board
+from .pieces.color import Color
+from .pieces.piece import Piece
+
+
+class Chess(Board):
+    def __init__(self):
+        super().__init__()
+
+
+
+
+
