b99815099294de43221a8a77d2b7fd5d46ae66af
Axel Colmant <axel.colmant@gmail.com>
2024-04-15 15:15:20 +0200

    Implement Rook methods

--- chess/pieces/rook.py
+++ chess/pieces/rook.py
@@ -1,6 +1,7 @@
 from .piece import Piece
 from .color import Color
 from ..board import Board
+from ..board.board import BoardException
 
 
 class Rook(Piece):
@@ -11,10 +12,34 @@ class Rook(Piece):
         self._board = board
 
     def move(self, end: tuple[int, int]):
-        pass
+        if self.canMove(end):
+            self._board.setCase(self.getPosition()[0], self.getPosition()[1], None)
+            self._board.setCase(end[0], end[1], self)
+            self._position = end
+        else:
+            raise BoardException(3)
 
     def getMoves(self) -> list[tuple[int, int]]:
-        pass
+        moves = []
+        x, y = self.getPosition()
+        for i in range(8):
+            if i != x and self.canMove((i, y)):
+                moves.append((i, y))
+            if i != y and self.canMove((x, i)):
+                moves.append((x, i))
+        return moves
 
     def canMove(self, end: tuple[int, int]):
-        pass
+        x1, y1 = self.getPosition()
+        x2, y2 = end
+
+        if x1 == x2:
+            for y in range(min(y1, y2) + 1, max(y1, y2)):
+                if self._board.getCases(x1, y) is not None:
+                    return False
+            return True
+        elif y1 == y2:
+            for x in range(min(x1, x2) + 1, max(x1, x2)):
+                if self._board.getCases(x, y1) is not None:
+                    return False
+            return True
